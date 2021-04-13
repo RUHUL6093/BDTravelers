d044b9c82a6fca5aa75c9d9b8f63ff1bb047f795
RUHUL AMIN <59334063+RUHUL6093@users.noreply.github.com>
2021-04-13 06:59:20 +0600

    new card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import Header from "./componentes/Header/Header";
+import Transport from "./componentes/Transports/transport";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Header></Header>
+      <Transport></Transport>
     </div>
   );
 }
