40d70e43bceee44d796142d06c93f90bde29b9ea
RUHUL AMIN <59334063+RUHUL6093@users.noreply.github.com>
2021-04-13 15:17:26 +0600

    update component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import Header from "./componentes/Header/Header";
 
 import MediaCard from "./componentes/Transport1/Transport";
 import "./styles.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export default function App() {
   return (
     <div className="App">
       <Header></Header>
-
       <div className="sty">
         <MediaCard></MediaCard>
         <MediaCard></MediaCard>
